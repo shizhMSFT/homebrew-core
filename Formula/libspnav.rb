fbd7820f75c3ed77a518221cabd7e0d5e06ec0ec
Porkepix <porkepix@gmail.com>
2022-04-02 16:21:26 +0200

    libspnav 1.0

--- Formula/libspnav.rb
+++ Formula/libspnav.rb
@@ -1,8 +1,8 @@
 class Libspnav < Formula
   desc "Client library for connecting to 3Dconnexion's 3D input devices"
   homepage "https://spacenav.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/spacenav/spacenav%20library%20%28SDK%29/libspnav%200.3/libspnav-0.3.tar.gz"
-  sha256 "e1f855f47da6e75bdec81fe4b67171406abaf342c6fe3208c78e13bf862a3f05"
+  url "https://downloads.sourceforge.net/project/spacenav/spacenav%20library%20%28SDK%29/libspnav%201.0/libspnav-1.0.tar.gz"
+  sha256 "8849b7f7826d750f6956cf8f4f53937f2359ab6da97d6c834c71d5f771212e7c"
 
   livecheck do
     url :stable
