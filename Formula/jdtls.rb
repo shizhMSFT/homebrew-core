962139d19f75eca2795b3c02a5dd614fd782a3c5
Porkepix <porkepix@gmail.com>
2022-07-02 03:52:42 +0200

    jdtls 1.13.0
    fix build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/jdtls.rb
+++ Formula/jdtls.rb
@@ -1,9 +1,9 @@
 class Jdtls < Formula
   desc "Java language specific implementation of the Language Server Protocol"
   homepage "https://github.com/eclipse/eclipse.jdt.ls"
-  url "https://download.eclipse.org/jdtls/milestones/1.12.0/jdt-language-server-1.12.0-202206011637.tar.gz"
-  version "1.12.0"
-  sha256 "38f20fec27a8fcdb1b3c49a117629dd0800b161171afd4b894bdc026f2fca2f3"
+  url "https://download.eclipse.org/jdtls/milestones/1.13.0/jdt-language-server-1.13.0-202206301721.tar.gz"
+  version "1.13.0"
+  sha256 "5a29ad28d765f408106a87b147b239696942a11f22f950c52dc37176fce9cfc9"
   license "EPL-2.0"
   version_scheme 1
 
@@ -16,8 +16,8 @@ class Jdtls < Formula
     sha256 cellar: :any_skip_relocation, all: "5af62f9cf5c42b7ba320b51901224ca87593475a80d292beeb3cbd9aede5e50f"
   end
 
-  depends_on "openjdk@11"
-  depends_on "python@3.9"
+  depends_on "openjdk"
+  depends_on "python@3.10"
 
   def install
     libexec.install "bin"
@@ -27,7 +27,7 @@ class Jdtls < Formula
     libexec.install "plugins"
 
     (bin/"jdtls").write_env_script libexec/"bin/jdtls",
-      Language::Java.overridable_java_home_env("11")
+      Language::Java.overridable_java_home_env
   end
 
   test do
