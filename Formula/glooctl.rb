e7b680ceb0b254e0f2c5653fe1338dc07a3f6c7b
Porkepix <porkepix@gmail.com>
2022-08-24 00:19:25 +0200

    glooctl 1.12.9

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.12.8",
-      revision: "0fb49a386d926285a92e3e74447c5defcdea4335"
+      tag:      "v1.12.9",
+      revision: "e7780879e941e682e4644b2224e07249029b27bf"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
