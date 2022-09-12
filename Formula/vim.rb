2d80026d87954b4c22d2c38a5a833944c5d6768a
Porkepix <porkepix@gmail.com>
2022-09-12 15:25:15 +0200

    vim 9.0.0450

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,10 +2,9 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v9.0.0350.tar.gz"
-  sha256 "36c211d34beb734fa322975daa170868d7172b1a5f9605257d532cebf956f708"
+  url "https://github.com/vim/vim/archive/v9.0.0450.tar.gz"
+  sha256 "7ff15a0e9fd33fffb61558b7e566171be3f1cd1413dcae53ac068204af699115"
   license "Vim"
-  revision 1
   head "https://github.com/vim/vim.git", branch: "master"
 
   # The Vim repository contains thousands of tags and the `Git` strategy isn't
